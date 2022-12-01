07220105f9a5cf824680660652421c3a88b6d388
Denis Bolshakov <Dysprosium92@ya.ru>
2022-12-01 08:27:48 +0300

    add clang format to provision.sh file

--- provision.sh
+++ provision.sh
@@ -2,9 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo 'Installing make'
+echo 'Installing Make'
 apt-get install make
 echo 'Installing Valgrind'
 sudo apt-get install valgrind
 echo 'Installing Cppcheck'
 apt-get -y install cppcheck
+echo 'Installing Clang-Format'
+apt-get install clang-format
